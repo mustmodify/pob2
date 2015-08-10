917547beaa34d922c6bc87ee9ce0b7d19558c7f8
Johnathon Wright <jw@mustmodify.com>
2015-08-10 12:45:31 -0500

    copy template from another project. Dashgum didn't play nice.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title( value=nil )
+    @page_title ||= value
+  end
+
+  def pad_options?
+    page_title.not.blank?
+  end
+
+  def row_id_for( object )
+    "#{object.class.name.tableize}_#{object.id}"
+  end
 end
