09e6d953d857ae71dff150995accf8bc22846c50
Johnathon Wright <jw@mustmodify.com>
2015-08-20 13:17:34 -0500

    do not collect position and rate on crew change form if the crew member is leaving.

--- app/models/crew_change.rb
+++ app/models/crew_change.rb
@@ -3,7 +3,7 @@ class CrewChange < ActiveRecord::Base
   belongs_to :employee
   belongs_to :position
 
-  validates_presence_of :date, :project, :action, :employee,
-                        :position, :rate
+  validates_presence_of :date, :project, :action, :employee
+  validates_presence_of :position, :rate, :if => lambda{|change| change.action == 'In'}
 
 end
