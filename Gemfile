d2fc05667f942e4bdbb67ab838d5fbadc7c5b674
Johnathon Wright <jw@mustmodify.com>
2015-08-27 15:44:36 -0500

    switch from sqlite3 to mysql2 gem because sqlite3 doesn't support 'if'

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
-gem 'sqlite3'
+gem 'mysql2'
 gem 'puma'
 
 # asset-related gems
--- db/schema.rb
+++ db/schema.rb
@@ -14,157 +14,157 @@
 ActiveRecord::Schema.define(version: 20150820183341) do
 
   create_table "certs", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.string   "description"
+    t.integer  "employee_id",        limit: 4
+    t.string   "description",        limit: 255
     t.date     "expires_on"
-    t.string   "image_file_name"
-    t.string   "string"
-    t.string   "image_content_type"
-    t.string   "image_file_size"
-    t.string   "integer"
-    t.string   "image_updated_at"
-    t.string   "datetime"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "string",             limit: 255
+    t.string   "image_content_type", limit: 255
+    t.string   "image_file_size",    limit: 255
+    t.string   "integer",            limit: 255
+    t.string   "image_updated_at",   limit: 255
+    t.string   "datetime",           limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   create_table "competencies", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.integer  "position_id"
-    t.decimal  "rate"
+    t.integer  "employee_id", limit: 4
+    t.integer  "position_id", limit: 4
+    t.decimal  "rate",                  precision: 10
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "compliments", force: :cascade do |t|
-    t.integer  "employee_id"
+    t.integer  "employee_id", limit: 4
     t.date     "date"
-    t.text     "note"
+    t.text     "note",        limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.string   "name"
-    t.string   "relationship"
-    t.string   "home_phone"
-    t.string   "cell_phone"
-    t.string   "alt_phone"
+    t.integer  "employee_id",  limit: 4
+    t.string   "name",         limit: 255
+    t.string   "relationship", limit: 255
+    t.string   "home_phone",   limit: 255
+    t.string   "cell_phone",   limit: 255
+    t.string   "alt_phone",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "crew_changes", force: :cascade do |t|
     t.date    "date"
-    t.integer "project_id"
-    t.string  "action"
-    t.integer "employee_id"
-    t.integer "position_id"
-    t.decimal "rate"
-    t.text    "note"
+    t.integer "project_id",  limit: 4
+    t.string  "action",      limit: 255
+    t.integer "employee_id", limit: 4
+    t.integer "position_id", limit: 4
+    t.decimal "rate",                      precision: 10
+    t.text    "note",        limit: 65535
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 255
   end
 
   create_table "departure_sites", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
-    t.text   "details"
+    t.string "name",     limit: 255
+    t.string "category", limit: 255
+    t.text   "details",  limit: 65535
   end
 
   create_table "employees", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "middle_name"
-    t.string   "last_name"
+    t.string   "first_name",            limit: 255
+    t.string   "middle_name",           limit: 255
+    t.string   "last_name",             limit: 255
     t.date     "dob"
-    t.string   "email"
-    t.string   "nationality"
-    t.string   "ssn"
-    t.string   "gsn"
+    t.string   "email",                 limit: 255
+    t.string   "nationality",           limit: 255
+    t.string   "ssn",                   limit: 255
+    t.string   "gsn",                   limit: 255
     t.boolean  "transportation_needed"
-    t.integer  "body_weight"
-    t.integer  "bag_weight"
+    t.integer  "body_weight",           limit: 4
+    t.integer  "bag_weight",            limit: 4
     t.boolean  "eligible_for_rehire"
-    t.string   "cell_phone"
-    t.string   "home_phone"
-    t.string   "alt_phone"
-    t.string   "street1"
-    t.string   "street2"
-    t.string   "city"
+    t.string   "cell_phone",            limit: 255
+    t.string   "home_phone",            limit: 255
+    t.string   "alt_phone",             limit: 255
+    t.string   "street1",               limit: 255
+    t.string   "street2",               limit: 255
+    t.string   "city",                  limit: 255
     t.string   "state",                 limit: 2
     t.string   "zipcode",               limit: 5
-    t.string   "ident_issuer"
-    t.string   "ident_number"
+    t.string   "ident_issuer",          limit: 255
+    t.string   "ident_number",          limit: 255
     t.date     "ident_issue_date"
     t.date     "ident_expiration_date"
-    t.string   "picture_file_name"
-    t.string   "picture_content_type"
-    t.integer  "picture_file_size"
+    t.string   "picture_file_name",     limit: 255
+    t.string   "picture_content_type",  limit: 255
+    t.integer  "picture_file_size",     limit: 4
     t.datetime "picture_updated_at"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "notes", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.text     "body"
-    t.integer  "author_id"
+    t.integer  "employee_id", limit: 4
+    t.text     "body",        limit: 65535
+    t.integer  "author_id",   limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "oil_cos", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 255
   end
 
   create_table "positions", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string  "name"
-    t.integer "oil_co_id"
-    t.integer "customer_id"
-    t.integer "departure_site_id"
-    t.integer "work_site_id"
+    t.string  "name",              limit: 255
+    t.integer "oil_co_id",         limit: 4
+    t.integer "customer_id",       limit: 4
+    t.integer "departure_site_id", limit: 4
+    t.integer "work_site_id",      limit: 4
     t.date    "start_date"
     t.date    "end_date"
   end
 
   create_table "reprimand_categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 255
   end
 
   create_table "reprimands", force: :cascade do |t|
-    t.integer  "employee_id"
-    t.string   "reprimand_category_id"
+    t.integer  "employee_id",           limit: 4
+    t.string   "reprimand_category_id", limit: 255
     t.date     "date"
-    t.text     "note"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.text     "note",                  limit: 65535
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "restrictions", force: :cascade do |t|
-    t.integer  "employee_id"
+    t.integer  "employee_id",             limit: 4
     t.date     "effective_date"
-    t.string   "title"
-    t.string   "description"
-    t.string   "attachment_file_name"
-    t.string   "attachment_content_type"
-    t.integer  "attachment_file_size"
+    t.string   "title",                   limit: 255
+    t.string   "description",             limit: 255
+    t.string   "attachment_file_name",    limit: 255
+    t.string   "attachment_content_type", limit: 255
+    t.integer  "attachment_file_size",    limit: 4
     t.datetime "attachment_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "active",              limit: 1,   default: true
+    t.string   "email",               limit: 255
+    t.string   "first_name",          limit: 255
+    t.string   "last_name",           limit: 255
+    t.boolean  "active",                          default: true
     t.string   "password_digest",     limit: 255
     t.string   "persistence_token",   limit: 255
     t.string   "perishable_token",    limit: 255
@@ -176,8 +176,8 @@ ActiveRecord::Schema.define(version: 20150820183341) do
   end
 
   create_table "work_sites", force: :cascade do |t|
-    t.string "name"
-    t.text   "details"
+    t.string "name",    limit: 255
+    t.text   "details", limit: 65535
   end
 
 end
