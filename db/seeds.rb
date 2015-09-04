9697514d76c08c050dab749575cfb72b492039de
Johnathon Wright <jw@mustmodify.com>
2015-09-04 14:21:34 -0500

    seed cert names

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,28 @@ if User.count == 0
     password_digest: "$2a$10$UsNvFntrm/cD15bHy/2LZ.4biQ0izM80Eij0ivPni42uC0ZUZSQbO").save(validate: false)
 end
 
+if CertName.count == 0
+  CertName.create(name: "Bloodborne Pathogens")
+  CertName.create(name: "BP License To Go Offshore")
+  CertName.create(name: "Diploma - High School")
+  CertName.create(name: "Drivers License")
+  CertName.create(name: "Fall Protection")
+  CertName.create(name: "First Aid - CPR - AED")
+  CertName.create(name: "First Aid - CPR - AED - Firewatch - HAZ")
+  CertName.create(name: "HAZWOPER")
+  CertName.create(name: "Hydrogen Sulfide")
+  CertName.create(name: "ISN")
+  CertName.create(name: "MrSafety")
+  CertName.create(name: "Rigging Safety")
+  CertName.create(name: "Rig Pass")
+  CertName.create(name: "Safe Gulf/Land")
+  CertName.create(name: "Social Security Card")
+  CertName.create(name: "Safe Gulf")
+  CertName.create(name: "SEMS")
+  CertName.create(name: "TWIC")
+  CertName.create(name: "Water Survival W/ HUET")
+end
+
 if Position.count == 0
   Position.create(name: 'General Labor')
   Position.create(name: 'Crane Operator')
