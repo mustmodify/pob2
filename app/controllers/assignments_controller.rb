2cae368f798f4a69841aa56608b654012bf6cf21
Johnathon Wright <jw@mustmodify.com>
2015-09-08 17:17:00 -0500

    add check_competencies! to the assignments#create action to make sure they don't go crazy on us.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,6 +7,17 @@ class AssignmentsController < NestedCRUDController
     Project
   end
 
+  def create
+    @assignment = Assignment.new( local_params )
+    @assignment.check_competence!
+
+    if( @assignment.save )
+      redirect_to target_on_create
+    else
+      render :action => 'new'
+    end
+  end
+
   def local_params
     params.require(:assignment).permit(:employee_id, :position_id, :daily_rate).merge(project_id: params[:project_id])
   end
