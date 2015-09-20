70c37338346c9d6ebf53cb79a3fb0acec01db6df
Johnathon Wright <jw@mustmodify.com>
2015-09-20 14:03:12 -0500

    closes #78 - compliments and reprimands should not require an image.

--- app/models/compliment.rb
+++ app/models/compliment.rb
@@ -4,5 +4,4 @@ class Compliment < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :original => "800x800>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_presence :image
 end
--- app/models/reprimand.rb
+++ app/models/reprimand.rb
@@ -6,5 +6,4 @@ class Reprimand < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :original => "800x800>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_presence :image
 end
