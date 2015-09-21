759aee43d41982dae56161441b319ad0b2462c34
Johnathon Wright <jw@mustmodify.com>
2015-09-21 09:12:28 -0500

    contacts#show was copied-and-pasted from elsewhere... totally didn't line up with fields from Contact. Plus there's no way to get there.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,10 @@ class ContactsController < NestedCRUDController
     employee_contacts_path( @employee )
   end
 
+  def show
+    redirect_to edit_employee_contact_path(params[:employee_id], params[:id])
+  end
+
   def local_params
     params.require(:contact).permit(:name, :relationship, :home_phone, :cell_phone, :alt_phone).merge(:employee_id => @employee.id)
   end
