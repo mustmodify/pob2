7511ce34e792ef9df523688ab0dd414647c2777b
Johnathon Wright <jw@mustmodify.com>
2015-09-23 09:20:43 -0500

    #70 - employee warnings include expiration date

--- app/presenters/employee_alert_presenter.rb
+++ app/presenters/employee_alert_presenter.rb
@@ -1,4 +1,5 @@
 class EmployeeAlertPresenter
+  include ActionView::Helpers::DateHelper
 
   def initialize(emp)
     @employee = emp
@@ -15,11 +16,19 @@ class EmployeeAlertPresenter
   def generate_alerts
     [].tap do |out|
       if employee.expired_certs.size > 0
-        out << {:title => 'Expired Certs', :message => "The following certifications are out of date: #{employee.expired_certs.map(&:description).join(', ')}"}
+        out << {:title => 'Expired Certs', :message => "The following certifications are out of date: #{employee.expired_certs.map do |cert|
+           "#{cert.description} (#{cert.expires_on})"
+        end.join(', ')}"}
       end
 
       if employee.ident_expiration_date && employee.ident_expiration_date <= Date.today
-        out << {:title => "Driver's Licence Expired", :message => "Please update the information on file for this employe's DL."}
+        if employee.ident_expiration_date.past?
+          msg = "expired #{time_ago_in_words(employee.ident_expiration_date)} ago"
+        else
+          msg = "expires #{time_ago_in_words(employee.ident_expiration_date)} from now"
+        end
+
+        out << {:title => "Driver's Licence Expired", :message => "Please update the information on file for this employe's DL, which #{msg}."}
       end 
     end
   end
