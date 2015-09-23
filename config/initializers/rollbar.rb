9f8780b753b9348e88666a454863bddcf4fde1a5
Johnathon Wright <jw@mustmodify.com>
2015-09-23 09:21:17 -0500

    do not report on errors in development.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,11 +5,9 @@ Rollbar.configure do |config|
   
   config.access_token = '281dbcacfde44844b3fb18cf79c83f48'
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
+  unless Rails.env.production?
     config.enabled = false
   end
-
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
