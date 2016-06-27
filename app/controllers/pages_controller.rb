48a2bf505d386095b3ba3a5ad65592dc919b05d2
Johnathon Wright <jw@mustmodify.com>
2016-06-27 09:00:44 -0500

    ongoing projects list has duplicates, probably because of the join.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def dashboard
-    @ongoing_projects = Project.joins(:jobs).where('jobs.onboarding_date <= ? AND jobs.offboarding_date >= ?', Date.today, Date.today).order('projects.name')
-@ongoing_projects.pluck('projects.id')
+    @ongoing_projects = Project.joins(:jobs).where('jobs.onboarding_date <= ? AND jobs.offboarding_date >= ?', Date.today, Date.today).order('projects.name').uniq
+
     @projects = Project.visible.where('projects.id NOT IN (?)', @ongoing_projects.pluck(:id) ).order('projects.name')
   end
 end
