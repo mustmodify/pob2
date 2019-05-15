6e7e521ffb902411e24dd107a8c2944022a085ab
Johnathon Wright <jw@mustmodify.com>
2019-05-15 18:58:12 -0500

    Remove extraneous paren

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,6 +1,10 @@
 class AssignmentsController < CRUDController
   private
 
+  def target_on_destroy
+    params[:return_to] || employees_path
+  end
+
   def local_params
     params.fetch(:assignment, {}).permit(:employee_id, :project_id, :position_id)
   end
