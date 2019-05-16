fc3a319d4f89172532a511b46fa6a1c1a356cae8
Johnathon Wright <jw@mustmodify.com>
2019-05-16 17:23:06 -0500

    Well that didn't work very well

--- db/migrate/20190516201922_add_employee_last_worked_date.rb
+++ db/migrate/20190516201922_add_employee_last_worked_date.rb
@@ -1,7 +1,6 @@
 class AddEmployeeLastWorkedDate < ActiveRecord::Migration
   def change
-    add_column :employees, :pool, :string, :after => :email
+    add_column :employees, :pool, :string, :after => :email, default: 'Neutral'
     add_column :employees, :last_worked_on, :date, after: :pool
-    Q.tell %|update employees SET pool = "#{POOL[2]}"|
   end
 end
