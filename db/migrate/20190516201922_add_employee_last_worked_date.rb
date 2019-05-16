ab2de61992bffa13caa81399ac982e9a212be34d
Johnathon Wright <jw@mustmodify.com>
2019-05-16 17:17:18 -0500

    Start all employees in 3rd group

--- db/migrate/20190516201922_add_employee_last_worked_date.rb
+++ db/migrate/20190516201922_add_employee_last_worked_date.rb
@@ -2,6 +2,6 @@ class AddEmployeeLastWorkedDate < ActiveRecord::Migration
   def change
     add_column :employees, :pool, :string, :after => :email
     add_column :employees, :last_worked_on, :date, after: :pool
-    Q.tell %|update employees SET pool = "#{POOL[1]}"|
+    Q.tell %|update employees SET pool = "#{POOL[2]}"|
   end
 end
