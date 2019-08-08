22b39c32ff20837db785669047bed28c876464bb
Johnathon Wright <jw@mustmodify.com>
2019-08-08 13:53:11 -0400

    reverse order of screenings

--- app/controllers/screenings_controller.rb
+++ app/controllers/screenings_controller.rb
@@ -1,7 +1,11 @@
 class ScreeningsController < NestedCRUDController
+  def collection
+    @screenings.order('date desc')
+  end
+
   def target_on_create
     employee_screenings_path(@employee)
-  end 
+  end
 
   def local_params
     params.fetch(:screening, {}).permit(:category, :outcome, :date, :image).merge(employee_id: params[:employee_id])
