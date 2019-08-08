967fa60c327ea2ba9ab88e713b1151a7ecd7e9b3
Johnathon Wright <jw@mustmodify.com>
2019-08-08 12:55:25 -0400

    add 'Med Hold' as an outcome for Physicals

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -2,12 +2,19 @@ class Screening < ActiveRecord::Base
   belongs_to :employee
 
   CATEGORIES = ['Alcohol', 'Drug', 'Physical']
-  OUTCOMES = ['Pass', 'Fail']
+  OUTCOMES = ['Pass', 'Fail', 'Med Hold']
 
   has_attached_file :image, :styles => { :original => "800x800>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => [/\Aimage\/.*\Z/, 'application/pdf']
+  validates_presence_of :outcome
 
   validates_presence_of :employee, :date, :outcome
   validates_inclusion_of :category, :in => CATEGORIES, :allow_blank => false
+  validate :check_outcome
 
+  def check_outcome
+    if outcome == 'Med Hold' && category != 'Physical'
+      self.errors[:outcome] << "Med Hold can only be used for physicals."
+    end
+  end
 end
