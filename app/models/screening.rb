a82150b6455f7a34e154beb1135f26182cba37d9
Johnathon Wright <jw@mustmodify.com>
2019-08-08 13:46:36 -0400

    Fix 'missing image' icon for screenings

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -4,7 +4,7 @@ class Screening < ActiveRecord::Base
   CATEGORIES = ['Alcohol', 'Drug', 'Physical']
   OUTCOMES = ['Pass', 'Fail', 'Med Hold']
 
-  has_attached_file :image, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :default_url => "/assets/file-missing.png"
   validates_attachment_content_type :image, :content_type => [/\Aimage\/.*\Z/, 'application/pdf']
   validates_presence_of :outcome
 
