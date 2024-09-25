ea39116147817daba7019d81acfd87687597495f
Johnathon Wright <jw@mustmodify.com>
2024-09-25 11:52:05 -0400

    some fixes for clients

--- db/migrate/20240919190519_create_clients.rb
+++ db/migrate/20240919190519_create_clients.rb
@@ -1,7 +1,7 @@
 class CreateClients < ActiveRecord::Migration
   def change
      create_table :clients do |t|
-       t.boolean :active
+       t.boolean :active, default: true
        t.string :name
      end
 
